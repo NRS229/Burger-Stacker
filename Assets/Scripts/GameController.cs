ebd1fa69b13ed20520b24689600e1c93a9331031
NRS229 <noah229@hotmail.com>
2020-05-22 15:27:19 -0600

    Adaptation for new UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,13 @@ public class GameController : MonoBehaviour
 {
     //Bool
     public static bool pause;
+    public static bool gameStarted;
+    //Rigidbodies
+    public Rigidbody2D rbBunUp;
+    public Rigidbody2D rbMeat;
+    public Rigidbody2D rbBunDown;
+    //GameObjects
+    public GameObject userInterfaceItems;
 
     void Start()
     {
@@ -16,6 +23,16 @@ public class GameController : MonoBehaviour
     {
         //Detect a touch
         if (Input.touchCount > 0 || Input.GetKeyDown("space")){
+            if(!gameStarted){
+                //Set the game to started
+                gameStarted = true;
+                //Burger jumps to grill
+                rbBunUp.AddForce(new Vector2(-100f, 300));
+                rbMeat.AddForce(new Vector2(-101f, 300));
+                rbBunDown.AddForce(new Vector2(-102f, 300));
+                //Deactivate UI
+                userInterfaceItems.SetActive(false);
+            }
             pause = false;
         }
 
