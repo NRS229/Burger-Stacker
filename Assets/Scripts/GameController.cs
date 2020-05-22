b905d01d5757fbc5cca8b3942c41bd5856855d15
NRS229 <noah229@hotmail.com>
2020-05-21 18:02:46 -0600

    Start Menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //Bool
+    public static bool pause;
+
+    void Start()
+    {
+        pause = true;
+    }
+
+    void Update()
+    {
+        //Detect a touch
+        if (Input.touchCount > 0 || Input.GetKeyDown("space")){
+            pause = false;
+        }
+
+        if(pause){
+            Time.timeScale = 0f;
+        }else{
+            Time.timeScale = 1f;
+        }
+    }
+}
