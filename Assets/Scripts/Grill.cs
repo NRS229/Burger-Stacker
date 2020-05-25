92d842219d0291cd3b9f7b5810e5c70e2c8cb6ac
NRS229 <noah229@hotmail.com>
2020-05-25 17:29:50 -0600

    Camera Transition

---
+++ Assets/Scripts/Grill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grill : MonoBehaviour
+{
+    //Cameras
+    public GameObject menuCamera;
+    public GameObject gameplayCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.name == "LowerBun") {
+            //Change cameras
+            menuCamera.SetActive(false);
+            gameplayCamera.SetActive(true);
+        }        
+    }
+}
